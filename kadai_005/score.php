3c06ae4b9bfa77e816b0941d1a935ca814266c22
saoriyoshizawa <sataaan28@gmail.com>
2025-06-21 22:09:11 +0900

    Add files via upload
    ファイル追加

---
+++ kadai_005/score.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       // 変数に値を代入する
+
+       $score =80+60+55+40+100+25+80+95+30+60;
+
+       // 変数の中身を出力する
+       echo $score;
+
+       echo '<br>';
+
+       $average =(80+60+55+40+100+25+80+95+30+60)/10;
+
+       echo $average;
+       ?>
+   </p>
+</body>
+
+</html>
